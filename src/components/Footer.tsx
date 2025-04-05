e9ac5451099de1f5b8cda4807a049e2daf721639
Manisha Mardi <mardimanisha101@gmail.com>
2025-04-05 08:30:22 +0530

    `Update Footer component styles: add text color to certain elements`
    Signed-off-by: Manisha Mardi <mardimanisha101@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,12 +7,12 @@ const Footer = () => {
                 <div className='w-full mx-auto flex justify-around text-sm px-24'>
                     <div>
                         <h2 className='text-2xl font-bold tracking-wider'>PIXEL MART</h2>
-                        <p className='text-xs mt-1'>Shop in 8-bit Glory!</p>
+                        <p className='text-xs mt-1 text-gray-500'>Shop in 8-bit Glory!</p>
                     </div>
 
                     <div>
                         <h3 className='text-lg uppercase font-bold'>Shop</h3>
-                        <ul className="mt-1 space-y-1">
+                        <ul className="mt-1 space-y-1 text-gray-500">
                             <li>All Collection</li>
                             <li>Vintage</li>
                             <li>Modern</li>
@@ -21,7 +21,7 @@ const Footer = () => {
 
                     <div>
                         <h3 className='text-lg uppercase font-bold'>COMPANY</h3>
-                        <ul className="mt-1 space-y-1">
+                        <ul className="mt-1 space-y-1 text-gray-500">
                             <li>About us</li>
                             <li>Contact</li>
                         </ul>
@@ -29,7 +29,7 @@ const Footer = () => {
 
                     <div>
                         <h3 className='text-lg uppercase font-bold'>Support</h3>
-                        <ul className="mt-1 space-y-1">
+                        <ul className="mt-1 space-y-1 text-gray-500">
                             <li>FAQs</li>
                             <li>Cookie Policy</li>
                             <li>Terms of use</li>

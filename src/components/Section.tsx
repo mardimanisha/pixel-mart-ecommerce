c248e238edfe479e54c8b370ed3e162d7db7e1fc
Manisha Mardi <mardimanisha101@gmail.com>
2025-04-11 10:09:10 +0530

    `Update tailwind-scrollbar-hide to version 2.0.0 and modify Section component to use it`
    Signed-off-by: Manisha Mardi <mardimanisha101@gmail.com>

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Product from './Product'
 import products from '../../public/data/product'
 import { ChevronLeft, ChevronRight } from 'lucide-react';
@@ -13,37 +13,58 @@ interface SectionProps {
 const Section: React.FC<SectionProps> = ({ title, filterType }) => {
 
     const scrollRef = useRef<HTMLDivElement>(null);
-
-
-    const scroll = (direction: 'left' | 'right') => {
-        if (scrollRef.current) {
-            const { scrollLeft, clientWidth } = scrollRef.current;
-            const scrollAmount = direction === 'left' ? -clientWidth : clientWidth;
-
-            scrollRef.current.scrollTo({
-                left: scrollLeft + scrollAmount,
-                behavior: 'smooth',
-            })
-        }
-    };
-    
+    const [canScrollLeft, setCanScrollLeft] = useState(false);
+    const [canScrollRight, setCanScrollRight] = useState(true);
+    const scrollBy = 300;
     
     const filteredProducts = products.filter(product => product[filterType] === true)
 
-    
+    const updateScrollButtons = () => {
+        const el = scrollRef.current;
+        if (!el) return;
+
+        setCanScrollLeft(el.scrollLeft > 0);
+        setCanScrollRight(el.scrollLeft + el.clientWidth < el.scrollWidth - 1);
+    }
+
+    const handleScroll = (direction: 'left' | 'right') => {
+        if (scrollRef.current) {
+            const scrollAmount = direction === 'left' ? -scrollBy : scrollBy;
+            scrollRef.current.scrollBy({ left: scrollAmount, behavior: 'smooth' })
+            
+            setTimeout(() => {
+                updateScrollButtons()
+            }, 300)
+        }
+    }
+
+    useEffect(() => {
+        const el = scrollRef.current;
+        if (!el) return;
+
+        updateScrollButtons()
+
+        el.addEventListener('scroll', updateScrollButtons);
+        window.addEventListener('resize', updateScrollButtons)
+
+        return () => {
+            el.removeEventListener('scroll', updateScrollButtons);
+            window.removeEventListener('resize', updateScrollButtons)
+        }
+    }, [])
 
     return (
         <div className='m-10 relative w-full'>
             <h1 className='text-2xl font-bold'>{title}</h1>
 
             <div className='relative'>
-                <button onClick={() => scroll('left')}
-                    className='cursor-pointer absolute left-[-25px] top-1/2 -translate-y-1/2 z-20 text-2xl font-bold bg-white border border-black px-2 py-1'>
-                    <ChevronLeft />
-                </button>
+                {canScrollLeft && (
+                    <button onClick={() => handleScroll('left')}
+                        className='absolute left-[-25px] top-1/2 -translate-y-1/2 z-20 text-2xl font-bold bg-white border border-black px-2 py-4 cursor-pointer opacity-60 hover:opacity-100 transition-opacity duration-300'>
+                        <ChevronLeft />
+                    </button>
+                )}
                 
-
-
                 <div ref={scrollRef} className='py-8 flex gap-6 overflow-x-auto scrollbar-hide scroll-smooth max-w-full'>
                     {filteredProducts.length > 0 ? (
                        filteredProducts.map((product) => (
@@ -56,11 +77,12 @@ const Section: React.FC<SectionProps> = ({ title, filterType }) => {
                     )}
                 </div>
 
-                
-                    <button onClick={() => scroll('right')}
-                        className='absolute right-[-25px] top-1/2 -translate-y-1/2 z-20 text-2xl font-bold bg-white border border-black px-2 py-1 cursor-pointer'>
+                {canScrollRight && (
+                    <button onClick={() => handleScroll('right')}
+                        className='absolute right-[-25px] top-1/2 -translate-y-1/2 z-20 text-2xl font-bold border border-black bg-white px-2 py-4 cursor-pointer opacity-60 hover:opacity-100 transition-opacity duration-300'>
                             <ChevronRight />
                     </button>
+                )}
                 
             </div>
         </div>
@@ -68,3 +90,5 @@ const Section: React.FC<SectionProps> = ({ title, filterType }) => {
 }
 
 export default Section
+
+

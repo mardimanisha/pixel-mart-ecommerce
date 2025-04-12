20302442959091bc59ccc41c2c83403defb3d07d
Manisha Mardi <mardimanisha101@gmail.com>
2025-04-12 15:49:19 +0530

    `Removed NavButton component from Header and Sidebar, commented out in Header`
    Signed-off-by: Manisha Mardi <mardimanisha101@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import Search from './Search'
-import NavButton from './NavButton'
+//import NavButton from './NavButton'
 
 const Header = () => {
     const [isClient, setIsClient] = useState(false);
@@ -15,9 +15,9 @@ const Header = () => {
     return (
         <header className='bg-amber-50 fixed top-0 left-0 z-50 w-full flex justify-between items-center p-4 pl-10 pr-10 border-b-5'>
             <h1 className='text-2xl font-bold'>Pixel Mart</h1>
-            <div className='flex gap-8 items-center'>
+            <div className='flex gap-8 items-center pr-22'>
                 <Search />
-                <NavButton />
+                {/*<NavButton />*/}
             </div>
         </header>
     )
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,13 +4,22 @@ import React from 'react'
 const Sidebar = () => {
     return (
         <aside className='bg-white fixed right-0 w-32 border-l-5 h-full py-30'>
-            <div className='w-full flex justify-center items-center'>
+            <div className='w-full flex flex-col gap-8 justify-center items-center'>
                 <svg viewBox="0 0 256 256" className="h-12 w-12 cursor-pointer">
                     <rect fill="none" height="100" width="100" />
                     <path
                          d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
                         fill={'#fff'}
                         stroke={'#000'}
+                        strokeWidth="20"
+                    />
+                </svg>
+
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    height="50px" viewBox="0 -960 960 960" width="50px"
+                    fill="#1f1f1f"
+                    className='cursor-pointer'>
+                    <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"
                         strokeWidth="20"
                     />
                 </svg>
